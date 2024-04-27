fcee880711776db83aea74d16177e4a4d5013584
BrahmaReddy-devops <brahma8834@gmail.com>
2024-04-27 13:13:21 +0530

    roboshop project

--- Frontend.sh
+++ Frontend.sh
@@ -6,5 +6,5 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp -r C:\brahma\robosho-project\roboshop\roboshop.conf /etc/nginx/default.d/
+cp -r /root/roboshop/roboshop.conf /etc/nginx/default.d/
 systemctl restart nginx
